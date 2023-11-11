fd882dda52c807f0545962adf21bd69422f322d7
Danny Armstrong <lenzflare@gmail.com>
2023-11-10 21:22:46 -0600

    updating models, serializers, etc

--- surfaceinterval/settings.py
+++ surfaceinterval/settings.py
@@ -112,11 +112,11 @@ DATABASES = {
     }
 }
 
+FIXTURES_DIR = 'surfaceintervalapi/fixtures/'
 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
