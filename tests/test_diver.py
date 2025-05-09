c55ae4cb82f2b6af76b37e9c9307d199ace433d1
Danny Armstrong <lenzflare@gmail.com>
2025-05-09 16:10:45 -0500

    added test print utils

--- tests/test_diver.py
+++ tests/test_diver.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from surfaceintervalapi.models import Diver
 from surfaceintervalapi.types import IMPERIAL_UNIT
-from tests.utils import print_test_info
+from tests.utils import print_test_info, print_assert_info, print_test_action
 
 
 class DiverTestCase(TestCase):
@@ -19,17 +19,24 @@ class DiverTestCase(TestCase):
 
     def test_create_user_and_diver(self):
         print_test_info("Testing User and Diver Creation")
+
+        print_test_action("Creating User")
         user = User.objects.create(
             username=self.username, first_name=self.first_name, last_name=self.last_name
         )
 
+        print_assert_info("user is created")
         self.assertIsNotNone(user)
+        print_assert_info("user's names match")
         self.assertEqual(user.username, self.username)
         self.assertEqual(user.first_name, self.first_name)
         self.assertEqual(user.last_name, self.last_name)
 
+        print_test_action("Creating Diver")
         diver = Diver.objects.create(user=user, default_gear_set=None, units=IMPERIAL_UNIT)
 
+        print_assert_info("Diver is created")
         self.assertIsNotNone(diver)
+        print_assert_info("Diver user matches and units match")
         self.assertEqual(diver.user, user)
         self.assertEqual(diver.units, IMPERIAL_UNIT)
--- tests/utils.py
+++ tests/utils.py
@@ -4,3 +4,11 @@ DELIMITER = "==========="
 def print_test_info(msg):
     statement = f"{DELIMITER} {msg} {DELIMITER}"
     print(statement)
+
+def print_assert_info(msg):
+    statement = f"🧪 Asserting that {msg}."
+    print(statement)
+
+def print_test_action(msg):
+    statement = f"\n🔱 {msg}"
+    print(statement)
