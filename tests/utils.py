d1228f785885968a940f5410c7653903c037bbe9
Danny Armstrong <lenzflare@gmail.com>
2025-05-09 16:11:44 -0500

    formatting

--- tests/utils.py
+++ tests/utils.py
@@ -5,9 +5,11 @@ def print_test_info(msg):
     statement = f"{DELIMITER} {msg} {DELIMITER}"
     print(statement)
 
+
 def print_assert_info(msg):
     statement = f"🧪 Asserting that {msg}."
     print(statement)
+
 
 def print_test_action(msg):
     statement = f"\n🔱 {msg}"
