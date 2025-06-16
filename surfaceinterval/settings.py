1514acf0516b8ef556bdb51acdd2fc64abe42542
Daniel Armstrong <danielarmstrong@Daniels-MacBook-Pro.local>
2025-06-16 00:54:59 -0500

    updating requirements

--- surfaceinterval/settings.py
+++ surfaceinterval/settings.py
@@ -242,7 +242,7 @@ TEST_RUNNER = "django.test.runner.DiscoverRunner"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": f"redis://{DB_HOST}:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
